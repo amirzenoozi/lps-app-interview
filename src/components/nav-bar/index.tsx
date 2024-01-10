d8377572a33ccbd6c0c6a0be60ee5928d98dab86
amirzenoozi <amirzenoozi72@gmail.com>
2024-01-10 16:43:13 +0100

    :sparkles: Feat: Add NavBar For Servers

---
+++ src/components/nav-bar/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.scss';
+import { NavLink } from 'react-router-dom';
+import Icon from '@icon-park/react/es/all';
+
+const NavBar = () => {
+	const serverList = ['server01', 'server02', 'server03', 'server04', 'server05'];
+	return (
+		<header className={'header'}>
+			<nav>
+				<NavLink
+					to={'/'}
+					className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }
+					data-tooltip-id="tooltip-area"
+					data-tooltip-content="Home"
+					data-tooltip-place="right"
+				>
+					<Icon type={'Home'} theme={'outline'} size={24} />
+				</NavLink>
+				{serverList.map((server, index) => {
+					return (
+						<NavLink
+							to={`/servers/${server}`}
+							className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }
+							data-tooltip-id="tooltip-area"
+							data-tooltip-content={`Server 0${index + 1}`}
+							data-tooltip-place="right"
+						>
+							<Icon type={'Server'} theme={'outline'} size={24} />
+						</NavLink>
+					);
+				})}
+			</nav>
+		</header>
+	);
+}
+
+export default NavBar;
--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavBar from '../../components/nav-bar';
 import './style.scss';
 
 const Layout = () => {
 	return (
 		<>
+			<NavBar />
 			<Outlet />
 		</>
 	);
