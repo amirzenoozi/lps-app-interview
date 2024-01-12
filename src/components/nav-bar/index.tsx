8dcf5037ea887323f5ab18a1cbc46702c561a31a
amirzenoozi <amirzenoozi72@gmail.com>
2024-01-12 14:36:43 +0100

    :zap: Feat: Update NavBar Icons

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.scss'
 import { NavLink } from 'react-router-dom'
-import Icon from '@icon-park/react/es/all'
+import { ChartPie, Server } from '@icon-park/react'
 
 const NavBar = () => {
 	const serverList = ['server01', 'server02', 'server03', 'server04', 'server05']
@@ -12,10 +12,10 @@ const NavBar = () => {
 					to={'/'}
 					className={({ isActive, isPending }) => isPending ? 'pending' : isActive ? 'active' : '' }
 					data-tooltip-id="tooltip-area"
-					data-tooltip-content="Home"
+					data-tooltip-content="Memory Aggregation"
 					data-tooltip-place="right"
 				>
-					<Icon type={'Home'} theme={'outline'} size={24} />
+					<ChartPie theme={'outline'} size={24} />
 				</NavLink>
 				{serverList.map((server, index) => {
 					return (
@@ -27,7 +27,7 @@ const NavBar = () => {
 							data-tooltip-content={`Server 0${index + 1}`}
 							data-tooltip-place="right"
 						>
-							<Icon type={'Server'} theme={'outline'} size={24} />
+							<Server theme={'outline'} size={24} />
 						</NavLink>
 					)
 				})}
