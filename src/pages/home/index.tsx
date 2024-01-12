629d634868a70e8adc7c77e2e7231e3f07d088cb
amirzenoozi <amirzenoozi72@gmail.com>
2024-01-12 14:39:17 +0100

    :sparkles: Feat: Memory Aggregated From All Servers

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,203 +1,61 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useTranslation } from 'react-i18next'
+import React, { useEffect, useState } from 'react'
 import Container from '../../components/container'
 import FlexCol from '../../components/flex-col'
 import FlexRow from '../../components/flex-row'
 import WidgetCard from '../../components/widget-card'
 import './style.scss'
-import { WebSocketConnection } from '../../modules/web-socket'
-import { type ApexOptions } from 'apexcharts'
+import useMemoryWebSocket from '../../Hooks/useMemoryWebSocket'
 import ReactApexChart from 'react-apexcharts'
+import { type ApexOptions } from 'apexcharts'
 
 function Home () {
-	const { t } = useTranslation()
-	const streamMemoryRef = useRef<any>(null)
-	const streamCPURef = useRef<any>(null)
-	const [isConnected, setIsConnected] = useState<boolean>(false)
-	const [cpuUsage, setCpuUsage] = useState<number[]>([])
 	const [memoryUsage, setMemoryUsage] = useState<number[]>([])
-	const [cpuUsageTimeline, setCpuUsageTimeline] = useState<any[]>([[], [], [], []])
-	const [prevTime, setPrevTime] = useState<number>(0)
+	const [freeMemory, setFreeMemory] = useState<number[]>([])
+	const firstServer = useMemoryWebSocket('wss://lps-monitoring.up.railway.app/realtime', 'server01')
+	const secondServer = useMemoryWebSocket('wss://lps-monitoring.up.railway.app/realtime', 'server02')
+	const thirdServer = useMemoryWebSocket('wss://lps-monitoring.up.railway.app/realtime', 'server03')
+	const forthServer = useMemoryWebSocket('wss://lps-monitoring.up.railway.app/realtime', 'server04')
+	const fifthServer = useMemoryWebSocket('wss://lps-monitoring.up.railway.app/realtime', 'server05')
 
 	useEffect(() => {
-		let socketClient = new WebSocketConnection({ baseURL: 'wss://lps-monitoring.up.railway.app/realtime' })
-
-		socketClient.onSocketOpened(() => {
-			if (!isConnected) {
-				socketClient.emitSocketMessage({ type: 'all', machine: 'server01', subscribe: true })
-			}
-		})
-
-		socketClient.onSocketMessage((msg: any) => {
-			if ('success' in JSON.parse(msg.data)) {
-				setIsConnected(JSON.parse(msg.data).success)
-			} else {
-				const message = JSON.parse(msg.data)
-
-				// It Will Update Every 1 Second Since We Have Repeated Data
-				if (message.timestamp - prevTime > 1000) {
-					setPrevTime(message.timestamp)
-					setCpuUsage(message.cpu.map((item: any) => item.usage))
-					setMemoryUsage((prev: any) => [...prev, [message.timestamp, message.memory.usedPercentage]])
-					setCpuUsageTimeline((prev: any) => {
-						return [
-							[...prev[0], [message.timestamp, message.cpu[0].usage]],
-							[...prev[1], [message.timestamp, message.cpu[1].usage]],
-							[...prev[2], [message.timestamp, message.cpu[2].usage]],
-							[...prev[3], [message.timestamp, message.cpu[3].usage]]
-						]
-					})
-				}
-			}
-		})
-
-		socketClient.onSocketErrored((err: any) => {
-			setIsConnected(false)
-			socketClient.closeConnection()
-			socketClient = new WebSocketConnection({ baseURL: 'wss://lps-monitoring.up.railway.app/realtime' })
-		})
-
-		return () => {
-			socketClient.closeConnection()
-		}
-	}, [])
-
-	useEffect(() => {
-		streamMemoryRef.current?.chart?.updateSeries([
-			{
-				data: memoryUsage.slice(Math.max(memoryUsage.length - 20, 0)),
-				name: 'Used Memory',
-				color: '#ec9b00'
-			}
+		setMemoryUsage([
+			normlizeData(firstServer.usage),
+			normlizeData(secondServer.usage),
+			normlizeData(thirdServer.usage),
+			normlizeData(forthServer.usage),
+			normlizeData(fifthServer.usage)
 		])
-	}, [memoryUsage])
-
-	useEffect(() => {
-		streamCPURef.current?.chart?.updateSeries([
-			{
-				data: cpuUsageTimeline[0].slice(Math.max(cpuUsageTimeline[0].length - 20, 0)) || [],
-				name: 'CPU #1',
-				color: '#003f5c'
-			}, {
-				data: cpuUsageTimeline[1].slice(Math.max(cpuUsageTimeline[1].length - 20, 0)) || [],
-				name: 'CPU #2',
-				color: '#58508d'
-			}, {
-				data: cpuUsageTimeline[2].slice(Math.max(cpuUsageTimeline[2].length - 20, 0)) || [],
-				name: 'CPU #3',
-				color: '#bc5090'
-			}, {
-				data: cpuUsageTimeline[3].slice(Math.max(cpuUsageTimeline[3].length - 20, 0)) || [],
-				name: 'CPU #4',
-				color: '#ff6361'
-			}
+		setFreeMemory([
+			normlizeData(firstServer.free),
+			normlizeData(secondServer.free),
+			normlizeData(thirdServer.free),
+			normlizeData(forthServer.free),
+			normlizeData(fifthServer.free)
 		])
-	}, [cpuUsageTimeline])
+	}, [firstServer, secondServer, thirdServer, forthServer, fifthServer])
 
-	const radialOptions: ApexOptions = {
-		chart: {
-			height: 350,
-			type: 'radialBar',
-			offsetY: -10,
-			animations: {
-				enabled: false,
-				easing: 'linear',
-				speed: 0
-			}
-		},
-		grid: {
-			padding: {
-				bottom: 10
-			}
-		},
-		plotOptions: {
-			radialBar: {
-				startAngle: -135,
-				endAngle: 135,
-				dataLabels: {
-					name: {
-						show: false,
-						fontSize: '16px',
-						color: undefined,
-						offsetY: 40
-					},
-					value: {
-						offsetY: 60,
-						fontSize: '16px',
-						color: undefined,
-						formatter: function (val: any) {
-							return val + '%'
-						}
-					}
-				}
-			}
-		},
-		fill: {
-			type: 'solid',
-			colors: [({ value }: any) => {
-				if (value < 20) {
-					return '#5e943b'
-				} else if (value >= 21 && value < 40) {
-					return '#a5ac3e'
-				} else if (value >= 41 && value < 60) {
-					return '#ffdc1b'
-				} else if (value >= 61 && value < 80) {
-					return '#ec9b00'
-				} else if (value >= 81) {
-					return '#d9534f'
-				}
-			}]
-		},
-		stroke: {
-			dashArray: 4
-		}
+	const normlizeData = (data: number) => {
+		return Math.round((data / 500) * 100)
 	}
-	const lineOptions: ApexOptions = {
+
+	const pieChartOptions: ApexOptions = {
 		chart: {
-			id: 'realtime',
-			height: 350,
-			type: 'line',
 			animations: {
-				enabled: false,
-				easing: 'linear',
-				dynamicAnimation: {
-					speed: 200
-				}
-			},
-			toolbar: {
-				show: false
-			},
-			zoom: {
 				enabled: false
 			}
 		},
-		grid: {
-			padding: {
-				bottom: 40
+		labels: ['Server 01', 'Server 02', 'Server 03', 'Server 04', 'Server 05'],
+		responsive: [{
+			breakpoint: 480,
+			options: {
+				chart: {
+					width: 200
+				},
+				legend: {
+					position: 'bottom'
+				}
 			}
-		},
-		dataLabels: {
-			enabled: false
-		},
-		stroke: {
-			curve: 'smooth'
-		},
-		markers: {
-			size: 0
-		},
-		xaxis: {
-			type: 'datetime'
-		},
-		yaxis: {
-			max: 100,
-			min: 0
-		},
-		legend: {
-			position: 'bottom',
-			horizontalAlign: 'center',
-			floating: true,
-			offsetY: 0
-		}
+		}]
 	}
 
 	return (
@@ -205,23 +63,14 @@ function Home () {
 			<section className={'page'}>
 				<Container>
 					<FlexRow>
-						{ cpuUsage.length > 0 && cpuUsage.map((item, index) => {
-							return (
-								<FlexCol xs={24} sm={12} md={6} key={`cpu-${index}`}>
-									<WidgetCard title={`CPU-#${index}`}>
-										<ReactApexChart options={{ ...radialOptions, labels: [`CPU-#${index}`] }} series={[item]} type="radialBar" height={'100%'} width={'100%'} />
-									</WidgetCard>
-								</FlexCol>
-							)
-						})}
-						<FlexCol xs={24}>
+						<FlexCol xs={12}>
 							<WidgetCard title={'Memory Usage'}>
-								<ReactApexChart ref={streamMemoryRef} options={lineOptions} type={'line'} series={memoryUsage} height={400} />
+								<ReactApexChart options={pieChartOptions} series={memoryUsage} type="pie" width={'100%'} />
 							</WidgetCard>
 						</FlexCol>
-						<FlexCol xs={24}>
-							<WidgetCard title={'All CPUs Usage'}>
-								<ReactApexChart ref={streamCPURef} options={lineOptions} type={'line'} series={cpuUsageTimeline} height={400} />
+						<FlexCol xs={12}>
+							<WidgetCard title={'Free Memory'}>
+								<ReactApexChart options={pieChartOptions} series={freeMemory} type="pie" width={'100%'} />
 							</WidgetCard>
 						</FlexCol>
 					</FlexRow>
