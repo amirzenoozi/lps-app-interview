f53b1aadc7ba41b0dcbedb883a955b39bb22dc17
amirzenoozi <amirzenoozi72@gmail.com>
2024-01-12 12:28:42 +0100

    :white_check_mark: Test: Fix Test Issues

--- src/components/widget-card/index.tsx
+++ src/components/widget-card/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Icon from '@icon-park/react/es/all'
 import './style.scss'
 import type WidgetCardProps from './type'
+import { HamburgerButton } from '@icon-park/react'
 
 const WidgetCard = ({ title, children }: WidgetCardProps) => {
 	return (
 		<div className={'widgetCard'}>
 			<div className={'widgetCard__header'}>
 				<p>{ title }</p>
-				<Icon type={'HamburgerButton'} theme={'outline'} size={'20'} />
+				<HamburgerButton theme={'outline'} size={'20'} />
 			</div>
 			<div className={'widgetCard__content'}>
 				{ children }
