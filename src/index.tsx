a0d159cd7f7c255f407881c90ce36f5fb8866187
amirzenoozi <amirzenoozi72@gmail.com>
2024-01-12 12:28:49 +0100

    :white_check_mark: Test: Fix Test Issues

--- src/index.tsx
+++ src/index.tsx
@@ -10,38 +10,38 @@ import '@icon-park/react/styles/index.css'
 import './i18n'
 
 const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-        errorElement: <Error />
-      },
-      {
-        path: '/servers/:serverId',
-        element: <SingleServer />,
-        errorElement: <Error />
-      }
-    ]
-  },
-  {
-    path: '/error',
-    element: <Error />,
-    errorElement: <Error />
-  },
-  {
-    path: '*',
-    element: <Error />,
-    errorElement: <Error />
-  }
+	{
+		element: <Layout/>,
+		children: [
+			{
+				path: '/',
+				element: <Home/>,
+				errorElement: <Error/>
+			},
+			{
+				path: '/servers/:serverId',
+				element: <SingleServer/>,
+				errorElement: <Error/>
+			}
+		]
+	},
+	{
+		path: '/error',
+		element: <Error/>,
+		errorElement: <Error/>
+	},
+	{
+		path: '*',
+		element: <Error/>,
+		errorElement: <Error/>
+	}
 ])
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-      <Tooltip id="tooltip-area" />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router}/>
+		<Tooltip id="tooltip-area"/>
+	</React.StrictMode>
 )
